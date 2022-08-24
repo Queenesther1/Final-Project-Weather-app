e158b92377cd0a462ffda36200104a8c55f6069a
Esther <estherfiyin@gmail.com>
2022-08-25 00:12:21 +0200

    Removed console.log

--- src/index.js
+++ src/index.js
@@ -72,7 +72,7 @@ function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "7a7d7d9cf2c10d11191b0e8a3e9ff3b0";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
+
   axios.get(apiUrl).then(displayForecast);
 }
 
